92bb169a71e6890fa3a4a2980603a0fb4fa29964
Audrey Lim <audim98@icloud.com>
2017-10-02 09:40:04 +0800

    Initial Commit

---
+++ Final Assesment Audrey July2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final Assesment Audrey July2017
+//
+//  Created by Audrey Lim on 02/10/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
